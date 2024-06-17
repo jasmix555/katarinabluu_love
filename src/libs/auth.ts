782eb1077045ee9eba7dceeb5833d51ea13161d6
AarJul <barongdong3@gmail.com>
2024-06-17 14:00:27 +0900

    feat: package add firebase

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -3,14 +3,14 @@ import {
   signInWithPopup,
   UserCredential,
   signOut,
-} from 'firebase/auth';
-import { auth } from './firebase';
+} from 'firebase/auth'
+import { auth } from './firebase'
 
 export const login = (): Promise<UserCredential> => {
-  const provider = new GoogleAuthProvider();
-  return signInWithPopup(auth, provider);
-};
+  const provider = new GoogleAuthProvider()
+  return signInWithPopup(auth, provider)
+}
 
 export const logout = (): Promise<void> => {
-  return signOut(auth);
-};
+  return signOut(auth)
+}
